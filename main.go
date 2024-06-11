91a3f69d20932d331c11b3f255d28b6a414bceeb
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2024-06-11 15:57:35 +0300

    add: spin a database connection

--- main.go
+++ main.go
@@ -2,18 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000" // Default port if PORT environment variable is not set
+	envs := map[string]string{
+		"DB_HOST":     os.Getenv("DB_HOST"),
+		"DB_USER":     os.Getenv("DB_USER"),
+		"DB_PASSWORD": os.Getenv("DB_PASSWORD"),
+		"DB_NAME":     os.Getenv("DB_NAME"),
+		"PORT":        os.Getenv("PORT"),
 	}
 
+	for k, v := range envs {
+		if envs[k] == "" {
+			log.Fatalf("%v doesn't exist", envs[v])
+		}
+	}
+
+	var port string
+	if envs["PORT"] == "" {
+		port = "8000" // Default port if PORT environment variable is not set
+	} else {
+		port = envs["PORT"]
+	}
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=5432 sslmode=disable", envs["DB_HOST"], envs["DB_USER"], envs["DB_PASSWORD"], envs["DB_NAME"])
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	log.Println("database is connected successfully")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		pid := fmt.Sprintf("Hello world! Process ID : %d, Port: %s", os.Getpid(), port)
+		pid := fmt.Sprintf("Hello world! Process ID : %d, Port: %s, Database: %v", os.Getpid(), port, db.Dialector.Name())
 		w.Write([]byte(pid))
 	})
 
